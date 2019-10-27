8508413ea2d682af47502c4a3d19827d86071bdb
Emre Demir <emreedemir@yahoo.com>
2019-10-27 19:29:07 +0300

    window size pinned

--- cheese-v2/Game.Designer.cs
+++ cheese-v2/Game.Designer.cs
@@ -28,132 +28,133 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
-			this.startButton = new System.Windows.Forms.Button();
-			this.resetButton = new System.Windows.Forms.Button();
-			this.mapSelect = new System.Windows.Forms.ComboBox();
-			this.modeSelect = new System.Windows.Forms.ComboBox();
-			this.mazeTable = new System.Windows.Forms.TableLayoutPanel();
-			this.update = new System.Windows.Forms.Timer(this.components);
-			this.SuspendLayout();
-			// 
-			// startButton
-			// 
-			this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.startButton.Location = new System.Drawing.Point(10, 629);
-			this.startButton.Name = "startButton";
-			this.startButton.Size = new System.Drawing.Size(185, 48);
-			this.startButton.TabIndex = 1;
-			this.startButton.Text = "Start";
-			this.startButton.UseVisualStyleBackColor = true;
-			this.startButton.Click += new System.EventHandler(this.startButton_Click);
-			// 
-			// resetButton
-			// 
-			this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.resetButton.Location = new System.Drawing.Point(725, 629);
-			this.resetButton.Name = "resetButton";
-			this.resetButton.Size = new System.Drawing.Size(185, 48);
-			this.resetButton.TabIndex = 1;
-			this.resetButton.Text = "Reset";
-			this.resetButton.UseVisualStyleBackColor = true;
-			this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
-			// 
-			// mapSelect
-			// 
-			this.mapSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.mapSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.mapSelect.ForeColor = System.Drawing.SystemColors.MenuText;
-			this.mapSelect.FormattingEnabled = true;
-			this.mapSelect.IntegralHeight = false;
-			this.mapSelect.ItemHeight = 16;
-			this.mapSelect.Items.AddRange(new object[] {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.mapSelect = new System.Windows.Forms.ComboBox();
+            this.modeSelect = new System.Windows.Forms.ComboBox();
+            this.mazeTable = new System.Windows.Forms.TableLayoutPanel();
+            this.update = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(10, 629);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(185, 48);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(725, 629);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(185, 48);
+            this.resetButton.TabIndex = 1;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // mapSelect
+            // 
+            this.mapSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.mapSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mapSelect.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.mapSelect.FormattingEnabled = true;
+            this.mapSelect.IntegralHeight = false;
+            this.mapSelect.ItemHeight = 16;
+            this.mapSelect.Items.AddRange(new object[] {
             "map1",
             "map2",
             "map3"});
-			this.mapSelect.Location = new System.Drawing.Point(207, 629);
-			this.mapSelect.Margin = new System.Windows.Forms.Padding(0);
-			this.mapSelect.Name = "mapSelect";
-			this.mapSelect.Size = new System.Drawing.Size(173, 24);
-			this.mapSelect.TabIndex = 2;
-			this.mapSelect.Tag = "";
-			this.mapSelect.SelectedIndexChanged += new System.EventHandler(this.mapSelect_SelectedIndexChanged);
-			// 
-			// modeSelect
-			// 
-			this.modeSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.modeSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-			this.modeSelect.ForeColor = System.Drawing.SystemColors.MenuText;
-			this.modeSelect.FormattingEnabled = true;
-			this.modeSelect.IntegralHeight = false;
-			this.modeSelect.ItemHeight = 16;
-			this.modeSelect.Items.AddRange(new object[] {
+            this.mapSelect.Location = new System.Drawing.Point(207, 629);
+            this.mapSelect.Margin = new System.Windows.Forms.Padding(0);
+            this.mapSelect.Name = "mapSelect";
+            this.mapSelect.Size = new System.Drawing.Size(173, 24);
+            this.mapSelect.TabIndex = 2;
+            this.mapSelect.Tag = "";
+            this.mapSelect.SelectedIndexChanged += new System.EventHandler(this.mapSelect_SelectedIndexChanged);
+            // 
+            // modeSelect
+            // 
+            this.modeSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.modeSelect.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.modeSelect.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.modeSelect.FormattingEnabled = true;
+            this.modeSelect.IntegralHeight = false;
+            this.modeSelect.ItemHeight = 16;
+            this.modeSelect.Items.AddRange(new object[] {
             "player",
             "computer",
             "player vs computer",
             "player vs player"});
-			this.modeSelect.Location = new System.Drawing.Point(207, 653);
-			this.modeSelect.Margin = new System.Windows.Forms.Padding(0);
-			this.modeSelect.Name = "modeSelect";
-			this.modeSelect.Size = new System.Drawing.Size(173, 24);
-			this.modeSelect.TabIndex = 2;
-			this.modeSelect.SelectedIndexChanged += new System.EventHandler(this.modeSelect_SelectedIndexChanged);
-			// 
-			// mazeTable
-			// 
-			this.mazeTable.ColumnCount = 15;
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.Location = new System.Drawing.Point(10, 10);
-			this.mazeTable.Margin = new System.Windows.Forms.Padding(0);
-			this.mazeTable.Name = "mazeTable";
-			this.mazeTable.RowCount = 10;
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
-			this.mazeTable.Size = new System.Drawing.Size(900, 600);
-			this.mazeTable.TabIndex = 0;
-			this.mazeTable.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.mazeTable_CellPaint);
-			// 
-			// update
-			// 
-			this.update.Tick += new System.EventHandler(this.update_Tick);
-			// 
-			// Game
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(920, 690);
-			this.Controls.Add(this.modeSelect);
-			this.Controls.Add(this.mapSelect);
-			this.Controls.Add(this.resetButton);
-			this.Controls.Add(this.startButton);
-			this.Controls.Add(this.mazeTable);
-			this.Name = "Game";
-			this.Text = "Game";
-			this.Load += new System.EventHandler(this.Game_Load);
-			this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
-			this.ResumeLayout(false);
+            this.modeSelect.Location = new System.Drawing.Point(207, 653);
+            this.modeSelect.Margin = new System.Windows.Forms.Padding(0);
+            this.modeSelect.Name = "modeSelect";
+            this.modeSelect.Size = new System.Drawing.Size(173, 24);
+            this.modeSelect.TabIndex = 2;
+            this.modeSelect.SelectedIndexChanged += new System.EventHandler(this.modeSelect_SelectedIndexChanged);
+            // 
+            // mazeTable
+            // 
+            this.mazeTable.ColumnCount = 15;
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.Location = new System.Drawing.Point(10, 10);
+            this.mazeTable.Margin = new System.Windows.Forms.Padding(0);
+            this.mazeTable.Name = "mazeTable";
+            this.mazeTable.RowCount = 10;
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 60F));
+            this.mazeTable.Size = new System.Drawing.Size(900, 600);
+            this.mazeTable.TabIndex = 0;
+            this.mazeTable.CellPaint += new System.Windows.Forms.TableLayoutCellPaintEventHandler(this.mazeTable_CellPaint);
+            // 
+            // update
+            // 
+            this.update.Tick += new System.EventHandler(this.update_Tick);
+            // 
+            // Game
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 690);
+            this.Controls.Add(this.modeSelect);
+            this.Controls.Add(this.mapSelect);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.mazeTable);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Game";
+            this.Text = "Game";
+            this.Load += new System.EventHandler(this.Game_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Game_KeyDown);
+            this.ResumeLayout(false);
 
 		}
 
